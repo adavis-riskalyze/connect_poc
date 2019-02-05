7282c7f3e9adef0ea5197d4dbd096404da462d7a
Avery Davis <adavis@riskalyze.com>
2019-02-05 15:43:25 -0500

    remove ui token and ide files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 class App extends Component {
     componentWillMount() {
-        var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWFkX29ubHkiOm51bGwsImZpZCI6ODAwMzc2MCwiaXAiOiIyMDguMTg0LjIzMi4xNTAiLCJ1aWQiOjgwODEyNjcsInN5bmNfdHlwZSI6ImFnZyIsIm9uZV90aW1lX3VzZSI6dHJ1ZSwic3ViIjoiODA4MTI2NyIsInR5cGUiOiJpZnJhbWUiLCJqdGkiOiJhZTQ1OWE0OTZhMTgyMzM5ODQ1M2MwYmVlZmRlNTA2OGYzOGZkNWIwIiwiZXhwIjoxNTQ5NDAwMzI3LCJpYXQiOjE1NDkzOTY3MjcsImlwX3Jlc3RyaWN0ZWQiOmZhbHNlfQ.RDjkU1VWR5YIh4mmjcVXAc0H3-dOW7TADbQs4XHY_vY';
+        var token = '... Quovo Generated UI token goes here ...';
         window.setupConnect(token);
     }
 
