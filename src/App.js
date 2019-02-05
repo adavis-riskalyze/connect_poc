218a9aac24381829bea6623989d111e8e5483161
Avery Davis <adavis@riskalyze.com>
2019-02-05 15:36:07 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    componentWillMount() {
+        var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWFkX29ubHkiOm51bGwsImZpZCI6ODAwMzc2MCwiaXAiOiIyMDguMTg0LjIzMi4xNTAiLCJ1aWQiOjgwODEyNjcsInN5bmNfdHlwZSI6ImFnZyIsIm9uZV90aW1lX3VzZSI6dHJ1ZSwic3ViIjoiODA4MTI2NyIsInR5cGUiOiJpZnJhbWUiLCJqdGkiOiJhZTQ1OWE0OTZhMTgyMzM5ODQ1M2MwYmVlZmRlNTA2OGYzOGZkNWIwIiwiZXhwIjoxNTQ5NDAwMzI3LCJpYXQiOjE1NDkzOTY3MjcsImlwX3Jlc3RyaWN0ZWQiOmZhbHNlfQ.RDjkU1VWR5YIh4mmjcVXAc0H3-dOW7TADbQs4XHY_vY';
+        window.setupConnect(token);
+    }
+
+    handleClick() {
+        window.openConnect();
+    }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <button id="quovo-connect" onClick={ this.handleClick }>Connect Your Accounts</button>
         </header>
       </div>
     );
